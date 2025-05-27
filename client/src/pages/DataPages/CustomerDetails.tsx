e1d2560d7fea4f0383322a231a844c21feedf9e1
alfaxad1 <alfaxadder@gmail.com>
2025-05-27 17:01:24 +0300

    Enhances Customer Details page with more information
    Adds more customer, referee, and guarantor details to the Customer Details page.
    
    Improves the layout and styling of the page for better readability and presentation of customer information.

--- client/src/pages/DataPages/CustomerDetails.tsx
+++ client/src/pages/DataPages/CustomerDetails.tsx
@@ -6,12 +6,18 @@ interface Referee {
   name: string;
   relationship: string;
   phone_number: string;
+  id_number: string;
 }
 
 interface Guarantor {
   name: string;
+  id_number: string;
   relationship: string;
   phone_number: string;
+  bussiness_location: string;
+  residence_details: string;
+  pass_photo: string;
+  id_photo: string;
   collaterals: Collateral[];
 }
 // Define the structure of a collateral
@@ -24,13 +30,20 @@ interface Customer {
   customer: {
     first_name: string;
     last_name: string;
+    middle_name?: string;
     phone: string;
     national_id: string;
-    address: string;
     occupation: string;
     monthly_income: number;
     passport_photo: string;
     national_id_photo: string;
+    date_of_birth: string;
+    gender: string;
+    address: string;
+    county: string;
+    business_name?: string;
+    business_location?: string;
+    residence_details?: string;
   };
   collaterals: Collateral[];
   referees: Referee[];
@@ -65,91 +78,311 @@ const CustomerDetails = () => {
 
   return (
     <div>
-      <h1>Customer Details</h1>
+      <h1 className="text-gray-800 text-title-sm dark:text-white/90 text-center mb-4">
+        Customer Details
+      </h1>
       {customerDetails ? (
         <div className="custom-scrollbar h-[450px] overflow-y-auto px-2 pb-3">
-          <div className="mt-7">
-            <h5 className="font-bold">Personal Information</h5>
+          <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 ">
+            <div className="border border-gray-400 rounded-xl dark:border-gray-800 p-4">
+              <h3 className="text-gray-800 text-xl dark:text-white/90 mb-4">
+                Personal Information
+              </h3>
+              <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-2 gap-4 mb-4">
+                <div>
+                  <h1 className="text-gray-800 text-md font-medium dark:text-white/90">
+                    Customer Name:
+                  </h1>
+                  <p>
+                    {customerDetails.customer.first_name}{" "}
+                    {customerDetails.customer.last_name}{" "}
+                    {customerDetails.customer.middle_name}
+                  </p>
+                </div>
 
-            <p>
-              Name: {customerDetails.customer.first_name}{" "}
-              {customerDetails.customer.last_name}
-            </p>
-            <p>Phone: {customerDetails.customer.phone}</p>
-            <p>National ID: {customerDetails.customer.national_id}</p>
-            <p>Address: {customerDetails.customer.address}</p>
-            <p>Occupation: {customerDetails.customer.occupation}</p>
-            <p>Monthly Income: {customerDetails.customer.monthly_income}</p>
+                <div>
+                  <h1 className="text-gray-800 text-md font-medium dark:text-white/90">
+                    Phone Number:
+                  </h1>
+                  <p>{customerDetails.customer.phone}</p>
+                </div>
 
-            <div className="flex gap-4 m-4">
-              <div className="w-36 h-40 overflow-hidden">
-                <img
-                  width={128}
-                  height={128}
-                  src={customerDetails.customer.passport_photo}
-                  alt="Customer passport photo"
-                  className="w-full h-full object-cover"
-                />
+                <div>
+                  <h1 className="text-gray-800 text-md font-medium dark:text-white/90">
+                    ID Number:
+                  </h1>
+                  <p>{customerDetails.customer.national_id}</p>
+                </div>
+
+                <div>
+                  <h1 className="text-gray-800 text-md font-medium dark:text-white/90">
+                    Occupation:
+                  </h1>
+                  <p>{customerDetails.customer.occupation}</p>
+                </div>
+
+                <div>
+                  <h1 className="text-gray-800 text-md font-medium dark:text-white/90">
+                    Monthly Income:
+                  </h1>
+                  <p>
+                    Ksh.{" "}
+                    {Math.floor(
+                      customerDetails.customer.monthly_income
+                    ).toLocaleString()}
+                  </p>
+                </div>
+
+                <div>
+                  <h1 className="text-gray-800 text-md font-medium dark:text-white/90">
+                    Address:
+                  </h1>
+                  <p>{customerDetails.customer.address}</p>
+                </div>
+
+                <div>
+                  <h1 className="text-gray-800 text-md font-medium dark:text-white/90">
+                    Date of Birth:
+                  </h1>
+                  <p>{customerDetails.customer.date_of_birth}</p>
+                </div>
+
+                <div>
+                  <h1 className="text-gray-800 text-md font-medium dark:text-white/90">
+                    Gender:
+                  </h1>
+                  <p>{customerDetails.customer.gender}</p>
+                </div>
+
+                <div>
+                  <h1 className="text-gray-800 text-md font-medium dark:text-white/90">
+                    Bussiness Name:
+                  </h1>
+                  <p>{customerDetails.customer.business_name}</p>
+                </div>
+
+                <div>
+                  <h1 className="text-gray-800 text-md font-medium dark:text-white/90">
+                    Business Location:
+                  </h1>
+                  <p>{customerDetails.customer.business_location}</p>
+                </div>
+
+                <div>
+                  <h1 className="text-gray-800 text-md font-medium dark:text-white/90">
+                    County:
+                  </h1>
+                  <p>{customerDetails.customer.county}</p>
+                </div>
+
+                <div>
+                  <h1 className="text-gray-800 text-md font-medium dark:text-white/90">
+                    Residence Details:
+                  </h1>
+                  <p>{customerDetails.customer.residence_details}</p>
+                </div>
               </div>
-              <div className="w-64 h-40 overflow-hidden">
-                <img
-                  width={128}
-                  height={128}
-                  src={customerDetails.customer.national_id_photo}
-                  alt="Customer national ID photo"
-                  className="w-full h-full object-cover"
-                />
+
+              <h3 className="text-gray-800 text-xl dark:text-white/90 mt-4">
+                Collaterals
+              </h3>
+              {customerDetails.collaterals.length > 0 ? (
+                customerDetails.collaterals.map(
+                  (collateral: Collateral, index: number) => (
+                    <div
+                      key={index}
+                      className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4"
+                    >
+                      <div>
+                        <h1 className="text-gray-800 text-md font-medium dark:text-white/90">
+                          Item Name:
+                        </h1>
+                        <p>{collateral.item_name}</p>
+                      </div>
+
+                      <div>
+                        <h1 className="text-gray-800 text-md font-medium dark:text-white/90">
+                          Number of Items:
+                        </h1>
+                        <p>{collateral.item_count}</p>
+                      </div>
+
+                      <div className="sm:col-span-2">
+                        <h1 className="text-gray-800 text-md font-medium dark:text-white/90">
+                          Additional Details:
+                        </h1>
+                        <p>{collateral.additional_details || "-"}</p>
+                      </div>
+                    </div>
+                  )
+                )
+              ) : (
+                <p>No collaterals available.</p>
+              )}
+
+              <div className="flex flex-col sm:flex-row gap-5">
+                <div className="w-36 h-40 overflow-hidden border border-gray-300 rounded-xl dark:border-gray-800">
+                  <img
+                    width={128}
+                    height={128}
+                    src={customerDetails.customer.passport_photo}
+                    alt="Customer passport photo"
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                <div className="w-64 h-40 overflow-hidden border border-gray-300 rounded-xl dark:border-gray-800">
+                  <img
+                    width={128}
+                    height={128}
+                    src={customerDetails.customer.national_id_photo}
+                    alt="Customer national ID photo"
+                    className="w-full h-full object-cover"
+                  />
+                </div>
               </div>
             </div>
-            <h5 className="font-bold mt-4">Collaterals</h5>
-            {customerDetails.collaterals.length > 0 ? (
-              customerDetails.collaterals.map(
-                (collateral: Collateral, index: number) => (
-                  <p key={index}>
-                    {collateral.item_name} - {collateral.item_count} (
-                    {collateral.additional_details})
-                  </p>
-                )
-              )
-            ) : (
-              <p>No collaterals available.</p>
-            )}
-            <h5 className="font-bold mt-4">Referees</h5>
-            {customerDetails?.referees.length > 0 ? (
-              customerDetails.referees.map((referee, index) => (
-                <p key={index}>
-                  {referee.name} - {referee.relationship} (
-                  {referee.phone_number})
-                </p>
-              ))
-            ) : (
-              <p>No referees available.</p>
-            )}
 
-            <h5 className="font-bold mt-4">Guarantors</h5>
-            {customerDetails?.guarantors.length > 0 ? (
-              customerDetails.guarantors.map((guarantor, index) => (
-                <div key={index}>
-                  <p>
-                    {guarantor.name} - {guarantor.relationship} (
-                    {guarantor.phone_number})
-                  </p>
-                  <h6 className="font-bold">Guarantor Collaterals</h6>
-                  {guarantor.collaterals.length > 0 ? (
-                    guarantor.collaterals.map((collateral, cIndex) => (
-                      <p key={cIndex}>
-                        {collateral.item_name} - {collateral.item_count} (
-                        {collateral.additional_details})
-                      </p>
-                    ))
-                  ) : (
-                    <p>No collaterals available.</p>
-                  )}
-                </div>
-              ))
-            ) : (
-              <p>No guarantors available.</p>
-            )}
+            <div className="border border-gray-400 rounded-xl dark:border-gray-800 p-4">
+              <h3 className="text-gray-800 text-xl dark:text-white/90 mt-4">
+                Guarantors
+              </h3>
+              {customerDetails?.guarantors.length > 0 ? (
+                customerDetails.guarantors.map((guarantor, index) => (
+                  <div key={index}>
+                    <div className="mb-4">
+                      <h1 className="text-gray-800 text-md font-medium dark:text-white/90">
+                        Name:
+                      </h1>
+                      <p>{guarantor.name}</p>
+                    </div>
+
+                    <div className="mb-4">
+                      <h1 className="text-gray-800 text-md font-medium dark:text-white/90">
+                        Relationship:
+                      </h1>
+                      <p>{guarantor.relationship}</p>
+                    </div>
+
+                    <div className="mb-4">
+                      <h1 className="text-gray-800 text-md font-medium dark:text-white/90">
+                        Phone Number:
+                      </h1>
+                      <p>{guarantor.phone_number}</p>
+                    </div>
+
+                    <div className="mb-4">
+                      <h1 className="text-gray-800 text-md font-medium dark:text-white/90">
+                        Residence Details:
+                      </h1>
+                      <p>{guarantor.residence_details || "-"}</p>
+                    </div>
+
+                    <div className="mb-4">
+                      <h1 className="text-gray-800 text-md font-medium dark:text-white/90">
+                        Business Location:
+                      </h1>
+                      <p>{guarantor.bussiness_location}</p>
+                    </div>
+
+                    <h6 className="text-gray-800 text-xl dark:text-white/90 mt-4">
+                      Guarantor Collaterals
+                    </h6>
+                    {guarantor.collaterals.length > 0 ? (
+                      guarantor.collaterals.map((collateral, cIndex) => (
+                        <div key={cIndex}>
+                          <div className="mb-4">
+                            <h1 className="text-gray-800 text-md font-medium dark:text-white/90">
+                              Item Name:
+                            </h1>
+                            <p>{collateral.item_name}</p>
+                          </div>
+
+                          <div className="mb-4">
+                            <h1 className="text-gray-800 text-md font-medium dark:text-white/90">
+                              Number of Items:
+                            </h1>
+                            <p>{collateral.item_count}</p>
+                          </div>
+
+                          <div className="mb-4">
+                            <h1 className="text-gray-800 text-md font-medium dark:text-white/90">
+                              Additional Details:
+                            </h1>
+                            <p>{collateral.additional_details || "-"}</p>
+                          </div>
+
+                          <div className="flex flex-col sm:flex-row gap-5">
+                            <div className="w-36 h-40 overflow-hidden border border-gray-300 rounded-xl dark:border-gray-800">
+                              <img
+                                width={128}
+                                height={128}
+                                src={guarantor.pass_photo}
+                                alt="Gurantor passport photo"
+                                className="w-full h-full object-cover"
+                              />
+                            </div>
+                            <div className="w-64 h-40 overflow-hidden border border-gray-300 rounded-xl dark:border-gray-800">
+                              <img
+                                width={128}
+                                height={128}
+                                src={guarantor.id_photo}
+                                alt="Gurantor national ID photo"
+                                className="w-full h-full object-cover"
+                              />
+                            </div>
+                          </div>
+                        </div>
+                      ))
+                    ) : (
+                      <p>No collaterals available.</p>
+                    )}
+                  </div>
+                ))
+              ) : (
+                <p>No guarantors available.</p>
+              )}
+            </div>
+
+            <div className="border border-gray-400 rounded-xl dark:border-gray-800 p-4">
+              <h3 className="text-gray-800 text-xl dark:text-white/90 mb-4">
+                Referees
+              </h3>
+              {customerDetails?.referees.length > 0 ? (
+                customerDetails.referees.map((referee, index) => (
+                  <div key={index} className="mb-4">
+                    <div className="mb-4">
+                      <h1 className="text-gray-800 text-md font-medium dark:text-white/90">
+                        Name:
+                      </h1>
+                      <p>{referee.name}</p>
+                    </div>
+
+                    <div className="mb-4">
+                      <h1 className="text-gray-800 text-md font-medium dark:text-white/90">
+                        ID Number:
+                      </h1>
+                      <p>{referee.id_number}</p>
+                    </div>
+
+                    <div className="mb-4">
+                      <h1 className="text-gray-800 text-md font-medium dark:text-white/90">
+                        Phone Number:
+                      </h1>
+                      <p>{referee.phone_number}</p>
+                    </div>
+
+                    <div className="mb-4">
+                      <h1 className="text-gray-800 text-md font-medium dark:text-white/90">
+                        Relationship:
+                      </h1>
+                      <p>{referee.relationship}</p>
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <p>No referees available.</p>
+              )}
+            </div>
           </div>
         </div>
       ) : (
